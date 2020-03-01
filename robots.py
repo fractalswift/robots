3901429fc6acf4f7d7e8585a1572e3f5e6ced18a
fractalswift <matthewthomasmacklin@gmail.com>
2020-03-01 21:01:50 +0000

    added return to fix bearing problem and added default mode

--- robots.py
+++ robots.py
@@ -23,12 +23,13 @@ instructions = [{"type": "asteroid", "size": {"x": 5, "y": 5}},
 
 
 class Asteroid:
-  def __init__(self, x, y):
+  def __init__(self, x, y, mode):
     self.x = x
     self.y = y
+    self.mode = mode
 
   def share_size(self):
-    return self.x, self.y
+    return self.x, self.y, self.mode
 
 class Robot:
   def __init__(self, x, y, bearing):
@@ -41,69 +42,96 @@ class Robot:
     self.moves_list.append(move)
 
   def turn_left(self):
+
     if self.bearing == 'north':
       self.bearing = 'west'
-      print('just changed bearing from north to west')
       print(f'calling self.bearing = {self.bearing}')
+      return
+
     if self.bearing == 'west':
       self.bearing = 'south'
+      print(f'calling self.bearing = {self.bearing}')
+      return
+
     if self.bearing == 'south':
       self.bearing = 'east'
+      print(f'calling self.bearing = {self.bearing}')
+      return
+
     if self.bearing == 'east':
       self.bearing = 'north'
+      print(f'calling self.bearing = {self.bearing}')
+      return
+
+
   
   
 
   def turn_right(self):
     if self.bearing == 'north':
       self.bearing = 'east'
+      print(f'calling self.bearing = {self.bearing}')
+      return
     if self.bearing == 'east':
       self.bearing = 'south'
+      print(f'calling self.bearing = {self.bearing}')
+      return
     if self.bearing == 'south':
       self.bearing = 'west'
+      print(f'calling self.bearing = {self.bearing}')
+      return
     if self.bearing == 'west':
       self.bearing = 'north'
+      print(f'calling self.bearing = {self.bearing}')
+      return
+
 
   def move_forward(self):
 
     if self.bearing == 'north':
       self.y += 1
+      print(f'calling self.y = {self.y}')
     if self.bearing == 'east':
       self.x += 1
+      print(f'calling self.x = {self.x}')
     if self.bearing == 'south':
       self.y -= 1
+      print(f'calling self.y = {self.y}')
     if self.bearing == 'west':
       self.x -= 1
+      print(f'calling self.x = {self.x}')
     
 
   def calculate_location(self, ast_size):
 
 
     for move in self.moves_list:
-      
+
       if move == 'turn-left':
         self.turn_left()
-        print(f'bearing is now {self.bearing} and position is x: {self.x}, y: {self.y}')
+        print(f'calling turn left, bearing = {self.bearing}  self.x = {self.x}, self.y = {self.y}')
       if move == 'turn-right':
         self.turn_right()
-        print(f'bearing is now {self.bearing} and position is x: {self.x}, y: {self.y}')
+        print(f'calling turn right, bearing = {self.bearing}  self.x = {self.x}, self.y = {self.y}')
       if move == 'move-forward':
         self.move_forward()
-        print(f'bearing is now {self.bearing} and position is x: {self.x}, y: {self.y}')
+        print(f'calling move forward, bearing = {self.bearing}  self.x = {self.x}, self.y = {self.y}')
 
     ast_x = ast_size[0]
     ast_y = ast_size[1]
-    x=0
-    y=0
+    ast_mode = ast_size[2]
+    x= self.x
+    y= self.y
 
-    if self.x >= ast_x:
-      x = self.x - ast_x
-    if self.y >= ast_y:
-      y = self.y - ast_y
-    if self.x <= 0:
-      x = self.x + ast_x
-    if self.y <= 0:
-      y = self.y + ast_y
+    if ast_mode == '3d':
+      if self.x >= ast_x:
+        x = self.x - ast_x
+      if self.y >= ast_y:
+        y = self.y - ast_y
+      if self.x <= 0:
+        x = self.x + ast_x
+      if self.y <= 0:
+        y = self.y + ast_y
 
     
     return {"type": "robot", "position": {"x": x, "y": y}, "bearing": self.bearing}
@@ -120,7 +148,8 @@ def processor(instructions):
     if row.get('type') == 'asteroid':
       ast_x = row.get('size').get('x')
       ast_y = row.get('size').get('y')
-      asteroid = Asteroid(ast_x, ast_y)
+      ast_mode = 'default'
+      asteroid = Asteroid(ast_x, ast_y, ast_mode)
       print(f'setting asteroid dimensions to x: {row.get("size").get("x")}')
       print(f'setting asteroid dimensions to y: {row.get("size").get("y")}')
 
